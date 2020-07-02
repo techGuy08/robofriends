a59d036a444ff7fb4f3c5359ea4aa57e6c9a0ace
korro21 <42660934+korro21@users.noreply.github.com>
2020-07-02 02:35:24 +0100

    initial update

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import "tachyons";
 import CardList from "./CardList";
-import { robots } from "../robots";
 import SearchBox from "./SearchBox";
-
-function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [allRobots, setAllRobots] = useState([]);
-  useEffect(() => {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: "",
+      robots: [],
+    };
+  }
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((users) => setAllRobots(users));
-  }, []);
-  const onSearchChange = (e) => {
+      .then((users) =>
+        this.setState({
+          robots: users,
+        })
+      );
+  }
+  onSearchChange = (e) => {
     const { value } = e.target;
-    setSearchTerm(value);
-    const filterRobots = robots.filter((el) =>
-      el.name.toLowerCase().includes(value.toLowerCase().trim())
-    );
-    setAllRobots(filterRobots);
+    this.setState({ search: value });
   };
-  return (
-    <div className="tc">
-      <div className="header">
-        <h1 className="headline_logo f2">RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange} value={searchTerm} />
+  render() {
+    const { search, robots } = this.state;
+    const filterRobots = robots.filter((el) =>
+      el.name.toLowerCase().includes(search.toLowerCase().trim())
+    );
+    return (
+      <div className="tc">
+        <div className="header">
+          <h1 className="headline_logo f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} value={search} />
+        </div>
+        {filterRobots.length === 0 && search ? (
+          <h1>Not Found</h1>
+        ) : (
+          <CardList robots={filterRobots} />
+        )}
       </div>
-      {allRobots.length === 0 && searchTerm ? (
-        <h1>Not Found</h1>
-      ) : (
-        <CardList robots={allRobots} />
-      )}
-    </div>
-  );
+    );
+  }
 }
+// function App() {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [allRobots, setAllRobots] = useState([]);
+//   async function getData() {
+//     return await fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((res) => res.json())
+//       .then((users) => users);
+//   }
+//   const robots = getData();
+//   console.log(robots);
+//   const onSearchChange = (e) => {
+//     const { value } = e.target;
+//     setSearchTerm(value);
+//     const filterRobots = robots.filter((el) =>
+//       el.name.toLowerCase().includes(value.toLowerCase().trim())
+//     );
+//     setAllRobots(filterRobots);
+//   };
+//   return (
+//     <div className="tc">
+//       <div className="header">
+//         <h1 className="headline_logo f2">RoboFriends</h1>
+//         <SearchBox searchChange={onSearchChange} value={searchTerm} />
+//       </div>
+//       {allRobots.length === 0 && searchTerm ? (
+//         <h1>Not Found</h1>
+//       ) : (
+//         <CardList robots={allRobots} />
+//       )}
+//     </div>
+//   );
+// }
 
 export default App;
